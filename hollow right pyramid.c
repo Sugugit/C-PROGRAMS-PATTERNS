6cf5e320d91fdd19b9b4466f76d10b77acd2a74a
Suganya <143573629+Sugugit@users.noreply.github.com>
2024-08-14 14:57:31 +0530

    Create hollow right pyramid.c

---
+++ hollow right pyramid.c
@@ -0,0 +1,33 @@
+CODE:
+
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for(i = 1; i <= rows; i++){
+        for(j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++) {
+            if(j == 1 || j == i || i == rows) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+OUTPUT:
+
+Enter the number of rows: 7
+      *
+     **
+    * *
+   *  *
+  *   *
+ *    *
+*******
